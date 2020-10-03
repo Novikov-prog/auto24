e917517bcd6956361b5f51ba20ea42ee01c661e0
Novikov <novikovprog@gmail.com>
2020-10-03 20:46:00 +0200

    connect sqlite DB with aiosqlite

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Response, status
+import aiosqlite
 
 
 
 app  = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    app.db_connection = await aiosqlite.connect('voitures_5_tables.db')
+
+@app.on_event("shutdown")
+async def shutdown():
+  await app.db_connection.close()
