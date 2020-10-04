0750669c9dbb55576fb8e255b1ba9a811dd11dd3
Novikov <novikovprog@gmail.com>
2020-10-04 18:30:42 +0200

    elimination de la DB + ajout script API

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ app  = FastAPI()
 
 @app.on_event("startup")
 async def startup():
-    app.db_connection = await aiosqlite.connect('voitures_5_tables.db')
+    app.db_connection = await aiosqlite.connect('.db')
 
 @app.on_event("shutdown")
 async def shutdown():
   await app.db_connection.close()
+
+@app.get("/marques")
+async def get_marques(page: int = 0, per_page: int = 10):
+  app.db_connection.row_factory = aiosqlite.Row
+  cursor = await app.db_connection.execute("SELECT * FROM file1 ORDER BY annonce_id LIMIT :per_page OFFSET :per_page*:page",
+      {'page': page, 'per_page': per_page})
+      marques = await cursor.fetchall()
+      return marques    
